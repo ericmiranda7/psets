0886708112f1f80a7d14bc5b67a5fa57a093b970
Eric <ericmiranda7@gmail.com>
2019-02-04 16:28:03 +0530

    Formatting, comments

--- extras/equaltriangle.c
+++ extras/equaltriangle.c
@@ -6,25 +6,27 @@ int main(void)
 {
     int theight[3];
     
+    // Take triangle sides input
     for (int i =0; i<3; i++)
     {
         printf("Enter side %i: ", i+1);
         scanf("%i", &theight[i]);
-        printf("%i", theight[i]);
     }
+    // Call validation function
     valid_tri(theight);
     
 }
 
 void valid_tri(int theight[3])
 {
+    // Comparing size of sides
     if (theight[0] + theight[1] <= theight[2])
     {
-        printf("Invalid Triangle!");
+        printf("Invalid Triangle!\n");
     }
     else
     {
-        printf("Valid Triangle.");
+        printf("Valid Triangle.\n");
     }
     
     
